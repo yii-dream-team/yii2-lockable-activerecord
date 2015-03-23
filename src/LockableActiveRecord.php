cebd05bc386cbc5308ca615290f4a1dfda7c6ca4
Alexey Samoylov <alexey.samoylov@gmail.com>
2015-03-23 16:09:47 +0700

    added composer.json

---
+++ src/LockableActiveRecord.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Alexey Samoylov <alexey.samoylov@gmail.com>
+ */
+namespace yiidreamteam\behaviors;
+
+use yii\base\Behavior;
+use yii\db\ActiveRecord;
+
+class LockableActiveRecord extends Behavior
+{
+	
+}
