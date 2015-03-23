d27ace395a2f36102c9b906e9cebb325e4a06b55
Alexey Samoylov <alexey.samoylov@gmail.com>
2015-03-23 17:51:34 +0700

    behavior

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # yii2-lockable-active-record
 Pessimistic locking behavior for Yii2 ActiveRecord
+
+## Installation ##
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+    php composer.phar require --prefer-dist yii-dream-team/yii2-jstree "*"
+
+or add
+
+    "yii-dream-team/yii2-jstree": "*"
+
+to the `require` section of your composer.json.
+
+## Usage ##
+Attach the behavior to your controller class.
+
+    public function behaviors()
+    {
+        return [
+            '\yiidreamteam\behaviors\LockableActiveRecord',
+        ];
+    }
+    
+Add @mixin phpdoc to you class definition.
+
+    /**
+     * Class Sample
+     * @package common\models
+     *
+     * @mixin \yiidreamteam\behaviors\LockableActiveRecord
+     */
+    class Wallet extends ActiveRecord { ... }
+    
+Use model locks in transaction.
+
+    $dbTransaction = $model->getDb()->beginTransaction(\yii\db\Transaction::SERIALIZABLE);
+    try {
+        $model->lockForUpdate();
+        $model->doSomeThingWhileLocked();
+        $dbTransaction->commit();
+    } catch(\Exception $e) {
+        $dbTransaction->rollBack();
+        throw $e;
+    }
+    
+## Licence ##
+
+MIT
+    
+## Links ##
+
+* http://yiidreamteam.com/yii2/lockable-active-record
+* https://dev.mysql.com/doc/refman/5.6/en/innodb-locking-reads.html
--- src/LockableActiveRecord.php
+++ src/LockableActiveRecord.php
@@ -1,13 +1,64 @@
 <?php
 /**
  * @author Alexey Samoylov <alexey.samoylov@gmail.com>
+ *
+ * @see https://dev.mysql.com/doc/refman/5.6/en/innodb-locking-reads.html
  */
 namespace yiidreamteam\behaviors;
 
+use Guzzle\Common\Exception\BadMethodCallException;
 use yii\base\Behavior;
+use yii\base\InvalidConfigException;
 use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
 
+/**
+ * Class LockableActiveRecord
+ * @package yiidreamteam\behaviors
+ */
 class LockableActiveRecord extends Behavior
 {
-	
+    const LOCK_SHARE_MODE = 'LOCK IN SHARE MODE';
+    const LOCK_FOR_UPDATE = 'FOR UPDATE';
+
+    /**
+     * @inheritdoc
+     */
+    public function attach($owner)
+    {
+        if (!$owner instanceof ActiveRecord)
+            throw new InvalidConfigException('Please attach this behavior to the instance of the ActiveRecord class');
+        parent::attach($owner);
+    }
+
+    public function lockForUpdate()
+    {
+        $this->lockInternal(self::LOCK_FOR_UPDATE);
+    }
+
+    public function lockShareMode()
+    {
+        $this->lockInternal(self::LOCK_SHARE_MODE);
+    }
+
+    private function checkTransaction()
+    {
+        if ($this->owner->getDb()->getTransaction() === null)
+            throw new BadMethodCallException('Running transaction is required');
+    }
+
+    /**
+     * @param string $param
+     * @throws \yii\db\Exception
+     */
+    private function lockInternal($param)
+    {
+        $this->checkTransaction();
+        /** @var ActiveRecord $model */
+        $model = $this->owner;
+        $pk = ArrayHelper::getValue($model::primaryKey(), 0);
+        $model->getDb()->createCommand('SELECT 1 FROM ' . $model->tableName() . ' WHERE ' . $pk . ' = :pk ' . $param, [
+            ':pk' => $model->getPrimaryKey(),
+        ])->execute();
+    }
 }
